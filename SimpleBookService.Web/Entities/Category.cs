b4fcb8124bf09957df594aebd170d24b78ec9d91
Maksuel Milhomem <mmilhomem@brainworks.com>
2023-06-20 21:44:58 +0100

    Creating Book and Category classes

---
+++ SimpleBookService.Web/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleBookService.Web.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Book> Books { get; set; }
+
+        public Category()
+        {
+            Books = new List<Book>();
+        }
+    }
+}
