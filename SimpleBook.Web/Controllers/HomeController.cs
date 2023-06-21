ab8581edcce613e1b358f415c129c9c53eeeb5d9
Maksuel Milhomem <mmilhomem@brainworks.com>
2023-06-21 16:06:36 +0100

    creating endpoint for client, html and js

---
+++ SimpleBook.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleBook.Web.Models;
+using System.Diagnostics;
+
+namespace SimpleBook.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View(id);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
